48be5d978129f4c3e1d5b53fdf89c901719d9100
THEParaence <128623447+THEPearaence@users.noreply.github.com>
2025-02-19 16:40:05 -0300

    app.componet update

--- gerenciador/src/app/app.component.ts
+++ gerenciador/src/app/app.component.ts
@@ -1,4 +1,46 @@
 import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo: string = 'Gerenciador de Tarefas';
+  novaTarefa: string = '';
+  tarefas: { nome: string; concluida: boolean }[] = [];
+  exibirLista: boolean = true;
+  title: any;
+
+  adicionarTarefa() {
+    if (this.novaTarefa.trim()) {
+      this.tarefas.push({ nome: this.novaTarefa, concluida: false });
+      this.novaTarefa = '';
+    }
+  }
+
+  alternarConclusao(tarefa: { nome: string; concluida: boolean }) {
+    tarefa.concluida = !tarefa.concluida;
+  }
+
+  toggleLista() {
+    this.exibirLista = !this.exibirLista;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*port { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -21,6 +63,7 @@ export class AppComponent {
   novaTarefa: string = '';
   tarefas: { nome: string; concluida: boolean }[] = [];
   exibirLista: boolean = true;
+  title: any;
 
   adicionarTarefa() {
     if (this.novaTarefa.trim()) {
@@ -37,4 +80,6 @@ export class AppComponent {
     this.exibirLista = !this.exibirLista;
   }
 }
+*/
+
 
